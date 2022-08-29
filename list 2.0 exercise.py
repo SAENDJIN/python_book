d770f1eaa16100986bd05f97625be9f009f586ea
SAENDJIN <andrey.saenko31@gmail.com>
2022-08-29 10:45:06 +0300

    Merge 2 project to one

---
+++ list 2.0 exercise.py
@@ -0,0 +1,19 @@
+Countries = ["USA", "Ireland", "UK", "Japan", "Netherlands"]
+
+print(Countries)
+
+print(sorted(Countries))
+
+print(Countries)
+
+print(sorted(Countries, reverse=True))
+
+print(Countries)
+
+Countries.reverse()
+print(Countries)
+
+Countries.sort()
+print(Countries)
+
+
---
+++ list 2.0.py
@@ -0,0 +1,22 @@
+neighbours = ["Max", "Rostyslav", "Lena", "Evgeniy", "Kristina", "Andrey"]
+print("This is original list:")
+print(neighbours)
+
+# Sorted не только временно сортирует список
+print("\nHere is sorted list:")
+print(sorted(neighbours))
+
+# reverse не сортирует в обратном алфавитном порядке как это делает sorted или sort
+# он просто разварачивает список на постоянно
+print("\nHere is sorted reversed list:")
+neighbours.reverse()
+print(neighbours)
+
+print("\nHere is original list again:")
+print(neighbours)
+
+print(f"\nHere is lenght of my neighbors: {len(neighbours)}")
+
+
+# neighbours.sort(reverse=True)
+# print(neighbours)

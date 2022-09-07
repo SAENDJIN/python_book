93ae14cc4b4d569307d487ad93c8f11d27bdd696
SAENDJIN <andrey.saenko31@gmail.com>
2022-09-07 16:00:30 +0300

    Add new files

---
+++ BookSection5/Mykitatask.py
@@ -0,0 +1,13 @@
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort(reverse = True)
+# print(thislist)
+#
+
+#
+# def myfunc(n):
+#     return abs(n - 50)
+#
+#
+# list1 = [100, 50, 65, 82, 23]
+# list1.sort(key=myfunc)
+# print(list1)
---
+++ BookSection5/if with list.py
@@ -0,0 +1,32 @@
+# requested_toppings = ['mushroms', 'green pepper', 'extra cheese']
+# requested_toppings = []
+#
+#
+# for requested_topping in requested_toppings:
+#     if requested_topping == 'green pepper':
+#         print(f'Sorry green pepper is ended.')
+#     else:
+#         print(f'Adding {requested_topping}.')
+#
+# print(f'\nFinnished making your pizza')
+#
+#
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         print(f'Adding {requested_topping}')
+#     print(f'\nFinnished making your pizza')
+# else:
+#     print('are u sure you want empty pizza?')
+
+
+available_toppings = ['mushrooms', 'olives', 'red pepper',
+                      'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'extra cheese', 'french fries']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Add {requested_topping}.")
+    else:
+        print(f"Sorry we dont have {requested_topping}")
+print('\nFinnished making your pizza')

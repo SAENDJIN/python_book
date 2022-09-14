65e2899dd6f4af37c1cdacdf6b9b671c3dbab67a
SAENDJIN <andrey.saenko31@gmail.com>
2022-09-14 18:30:13 +0300

    Add 2 new files from chapter 7 Add section book 7

---
+++ BookSection7/While.py
@@ -0,0 +1,46 @@
+# message =input('tell me somethin, and I will repeat it back to you: ')
+# print(message)
+
+# ------------------------------
+
+# name = input('Please enter your name: ')
+# print(f"\nHello, {name}")
+
+# ------------------------------
+
+# prompt = 'If you tell us who you are, we can personalize he message you see'
+# prompt += '\nWhat is your first name: '
+#
+# name = input(prompt)
+# print(f'Hello, {name}!')
+
+# ------------------------------
+
+# age = input("How old are you? ")
+# age = int(age)
+# print(age)
+
+# ------------------------------
+
+# height = input('How tall are you, in centimeters: ')
+# height = int(height)
+#
+# if height >= 160:
+#     print("You are tall enough to ride")
+# else:
+#     print("You are not tall enough to ride")
+
+# ------------------------------
+
+# num = 5 % 3
+# print(num)
+
+# ------------------------------
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+
+if number % 2== 0:
+    print(f"\nThe number {number} is even.")
+else:
+    print(f"\nThe number {number} is odd.")
---
+++ BookSection7/WhileEx.py
@@ -0,0 +1,23 @@
+# 7-1
+
+# car = input("Hey, what car do you want to rent? ")
+# print(f"\nLet me see if I can find you a {car}")
+
+# 7-2
+
+# person_num = input("Hey, how much person come to us with u? ")
+# person_num = int(person_num)
+# person_num = person_num + 1
+# if person_num > 8:
+#     print("you should wait")
+# else:
+#     print("Your place is ready")
+
+# 7-3
+
+# num = input("Enter a number, and I'll tell you if this number multiple to 10: ")
+# num = int(num)
+# if num % 10:
+#     print("No its not")
+# else:
+#     print("Yes it multiple")

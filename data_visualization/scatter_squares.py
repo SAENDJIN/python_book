b2f18689d911702eb05e75f22ed4d30fb2d13948
SAENDJIN <andrey.saenko31@gmail.com>
2023-07-16 16:13:57 +0300

    first part

---
+++ data_visualization/scatter_squares.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 1001)
+y_values = [x ** 2 for x in x_values]
+
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+# ax.scatter(x_values, y_values, s=10, c='purple')
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.cool, s=10)
+
+
+# Задати назву для графіка та кожної з осей
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Задати розмір шрифту для підписів на розмітці.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Задати діапазон для кожної з осейю
+ax.axis([0, 1100, 0, 1100000])
+
+# plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
